f501fee3cb7995a605130ed8ef8738ac1a8a270d
Jose <ls.joseignacio@gmail.com>
2025-10-24 06:55:39 +0000

    add multiple sheets per signature

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
+import math
+
 from pypdf import PdfWriter, PdfReader, PageObject
 
-output_pdf = PdfWriter()
 input_file = "input.pdf"
 output_file = input_file.replace(".pdf", "_print.pdf")
 
 
-def get_ordered_indexes(total_pages):
+def get_ordered_indexes(total_pages, sheets_per_signature=1):
     indexes = []
-    for page in range(0, total_pages, 4):
-        p1 = page + 3
-        p2 = page
-        p3 = page + 1
-        p4 = page + 2
-        for page_index in [p1, p2, p3, p4]:
-            if page_index < total_pages:
-                indexes.append(page_index)
-            else:
-                indexes.append(None)
+    pages_per_signature = sheets_per_signature * 4
+    number_of_signatures = math.ceil(total_pages / pages_per_signature)
+    for signature_index in range(number_of_signatures):
+        for side_index in range(sheets_per_signature * 2):
+            base_index = signature_index * pages_per_signature
+            even_modifier = sheets_per_signature * 4 - side_index - 1
+            odd_modifier = side_index
+            left_page = (base_index + (even_modifier if side_index % 2 == 0 else odd_modifier))
+            right_page = (base_index + (odd_modifier if side_index % 2 == 0 else even_modifier))
+            if left_page >= total_pages:
+                left_page = None
+            if right_page >= total_pages:
+                right_page = None
+            indexes += [
+                left_page, right_page
+            ]
     return indexes
 
 
-with open(input_file, 'rb') as readfile:
-    input_pdf = PdfReader(readfile)
-
-    page_width = input_pdf.pages[0].mediabox.width
-    page_height = input_pdf.pages[0].mediabox.height
-    page_layout = [[0, 0], [page_width, 0]]
-
-    pdf_length = len(input_pdf.pages)
-    ordered_indexes = get_ordered_indexes(pdf_length)
-
+def create_ordered_pdf(reference_pdf):
+    generated_pdf = PdfWriter()
     for i in range(0, len(ordered_indexes), 2):
         new_page = PageObject.create_blank_page(width=page_width * 2, height=page_height)
         for delta_index, coords in enumerate(page_layout):
@@ -41,9 +40,27 @@ with open(input_file, 'rb') as readfile:
             if ordered_indexes[index] >= pdf_length:
                 continue
 
-            new_page.merge_translated_page(input_pdf.pages[ordered_indexes[index]], coords[0], coords[1])
+            new_page.merge_translated_page(reference_pdf.pages[ordered_indexes[index]], coords[0], coords[1])
 
-        output_pdf.add_page(new_page)
+        generated_pdf.add_page(new_page)
+    return generated_pdf
 
-    with open(output_file, "wb") as writefile:
-        output_pdf.write(writefile)
+
+try:
+    with open(input_file, 'rb') as readfile:
+        input_pdf = PdfReader(readfile)
+
+        page_width = input_pdf.pages[0].mediabox.width
+        page_height = input_pdf.pages[0].mediabox.height
+        page_layout = [[0, 0], [page_width, 0]]
+
+        pdf_length = len(input_pdf.pages)
+        ordered_indexes = get_ordered_indexes(pdf_length, 2)
+
+        output_pdf = create_ordered_pdf(input_pdf)
+
+        with open(output_file, "wb") as writefile:
+            output_pdf.write(writefile)
+except FileNotFoundError:
+    print(f"File '{input_file}' not found")
+    
